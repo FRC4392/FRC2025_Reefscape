ef2d8b549bc6872f5974e677a8af9d82e9bdd4a8
Kristopher Zakfeld <kzakfeld@gmail.com>
2025-02-04 17:12:47 -0500

    subsystem files created

--- src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVisionSim.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVisionSim.java
@@ -42,7 +42,6 @@ public class VisionIOPhotonVisionSim extends VisionIOPhotonVision {
       // Add sim camera
       var cameraProperties = new SimCameraProperties();
 
-      
 
       cameraSim = new PhotonCameraSim(camera, cameraProperties);
       cameraSim.enableDrawWireframe(true);
