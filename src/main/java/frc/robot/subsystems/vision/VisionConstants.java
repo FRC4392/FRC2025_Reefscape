feacf2b377d4419e9a8559467bb1c8afa57db138
Braedonk <braedonkemp@gmail.com>
2025-03-07 18:26:23 -0500

    Friday's Adjustments

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -16,7 +16,7 @@ public class VisionConstants {
       AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
   // Camera names, must match names configured on coprocessor
-  public static String camera0Name = "limelight-reef";
+  public static String camera0Name = "limelight-center";
   public static String camera1Name = "limelight-left";
 
   // Robot to camera transforms
