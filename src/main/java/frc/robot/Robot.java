41ab2326a9ce8b1bc4bbe787e6c0b035052578b3
kzakfeld <kzakfeld@gmail.com>
2025-03-18 21:14:50 -0400

    PathPlanner Warmup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import com.ctre.phoenix6.SignalLogger;
+import com.pathplanner.lib.commands.FollowPathCommand;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.littletonrobotics.junction.LogFileUtil;
@@ -77,6 +79,11 @@ public class Robot extends LoggedRobot {
     CommandScheduler.getInstance().run();
 
     // Threads.setCurrentThreadPriority(false, 10);
+  }
+
+  @Override
+  public void robotInit() {
+      FollowPathCommand.warmupCommand().schedule();
   }
 
   @Override
